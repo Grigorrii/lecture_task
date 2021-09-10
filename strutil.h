0e9142040cb97b97fc57807249103a6cc9e6232d
Grigorrii <73611051+Grigorrii@users.noreply.github.com>
2021-09-10 21:50:08 +0300

    Create strutil.h

---
+++ strutil.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "struril.h"
+using namespace std;
+
+int main()
+{
+    char a[40], str[120];
+    cin >> a;
+    cin>> str;
+    cout << strConteins(a, str);
+    return 0;
+}
