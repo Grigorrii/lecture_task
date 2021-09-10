dff102077c6b57db4f344f22c5731f4b3b6066ea
Grigorrii <73611051+Grigorrii@users.noreply.github.com>
2021-09-10 21:57:35 +0300

    Update strutil.h

--- strutil.h
+++ strutil.h
@@ -1,12 +1,3 @@
-#include <iostream>
-#include "struril.h"
-using namespace std;
+#pragma once
 
-int main()
-{
-    char a[40], str[120];
-    cin >> a;
-    cin>> str;
-    cout << strConteins(a, str);
-    return 0;
-}
+int strConteins(char* a, char* str);
